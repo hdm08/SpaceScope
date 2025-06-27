94e06b6466c429c180430b288561920537bb1d5c
HARSHDEEP MAHAJAN <65184650+hdm08@users.noreply.github.com>
2025-06-27 14:52:30 +0100

    updateing nasa ai agent to skai

--- skai/skai/urls.py
+++ skai/skai/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for nasa_ai_agent project.
+URL configuration for skai project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
