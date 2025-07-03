50a5f7f5ab5fecca33a9b733c7225327f9bc420a
HARSHDEEP MAHAJAN <65184650+hdm08@users.noreply.github.com>
2025-07-03 09:20:37 +0100

    fixed preview build issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   server: {
     allowedHosts: ["space-scope-liard.vercel.app", "localhost:5173"], 
   },
-})
+  build: {
+    outDir: 'dist', 
+    emptyOutDir: true, 
+  },
+});
